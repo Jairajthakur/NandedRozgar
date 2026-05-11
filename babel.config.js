83330a97875a52e51675d3fc54153aaef6c8a096
Jairaj Singh <118998121+Jairajthakur@users.noreply.github.com>
2026-05-11 10:42:15 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      // Required for react-native-gesture-handler to work correctly
-      'react-native-reanimated/plugin',
-    ],
+    plugins: [],
   };
 };
