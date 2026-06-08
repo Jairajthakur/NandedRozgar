c435ff986e2893c6fec5fff6e4a26623885c0bfa
Jairaj Singh <118998121+Jairajthakur@users.noreply.github.com>
2026-06-08 15:48:27 +0530

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -17,7 +17,7 @@ export default ({ config }) => ({
 
   android: {
     package: "com.cityplus.app",
-    versionCode: parseInt(process.env.ANDROID_VERSION_CODE) || 64,
+    versionCode: parseInt(process.env.ANDROID_VERSION_CODE) || 65,
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#ffffff",
